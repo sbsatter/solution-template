d3dc4b33f86b058811a2ff5592be9e14ef27d93b
Shahed Bin Satter <shahed.satter@datasoft-bd.com>
2019-02-19 17:11:31 +0600

    Input skeleton defined

--- src/main/java/com/tigerit/exam/Solution.java
+++ src/main/java/com/tigerit/exam/Solution.java
@@ -1,6 +1,8 @@
 package com.tigerit.exam;
 
 
+import java.util.*;
+
 import static com.tigerit.exam.IO.*;
 
 /**
@@ -10,17 +12,76 @@ import static com.tigerit.exam.IO.*;
  * application's execution points start from inside run method.
  */
 public class Solution implements Runnable {
-    @Override
+	private static final int QUERY_MAX_LINE_NUM = 4;
+	
+	@Override
     public void run() {
         // your application entry point
 
         // sample input process
-        String string = readLine();
-
-        Integer integer = readLineAsInteger();
+//        String string = readLine();
+//
+//        Integer integer = readLineAsInteger();
 
         // sample output process
-        printLine(string);
-        printLine(integer);
+//        printLine(string);
+//        printLine(integer);
+	   
+	    Integer numberOfTestCases = readLineAsInteger();
+	    
+	    testCase(numberOfTestCases);
+	   
+	   
+	   
     }
+	
+	private void testCase(Integer caseNumber) {
+    	printLine(String.format("Test: %d", caseNumber));
+    	Integer numberOfTables = readLineAsInteger();
+		for (int tableNumber = 0; tableNumber < numberOfTables; tableNumber++) {
+			initiateTable(numberOfTables);
+		}
+		
+		Integer numberOfQueries = readLineAsInteger();
+		List<String> queries = new ArrayList<>();
+		for (int queryNumber = 0; queryNumber < numberOfQueries; queryNumber++) {
+			String query = collectQuery();
+			queries.add(query);
+		}
+	}
+	
+	private String collectQuery() {
+		StringBuilder builder = new StringBuilder();
+		for (int lineNumber = 0; lineNumber < QUERY_MAX_LINE_NUM; lineNumber++) {
+			builder.append(readLine());
+		}
+		readLine(); // empty input line
+		return builder.toString();
+	}
+	
+	private void initiateTable(Integer numberOfTables) {
+		String tableName = readLine();
+		// dimensions -> row, cols
+		int[] dimensions = Arrays.stream(readLine().split("\\s")).mapToInt(Integer::parseInt).toArray();
+		printLine(String.format("TEST: Dimensions: %d, %d", dimensions[0], dimensions[1]));
+		printLine(String.format("TEST: Table name: %s", tableName));
+		String [] columnNames = readLine().split("\\s");
+		printLine("TEST: Collected column names");
+		for (int row = 0; row < dimensions[0]; row++) {
+			int[] rowData = Arrays.stream(readLine().split("\\s")).mapToInt(Integer::parseInt).toArray();
+		}
+		
+		
+	}
+	
+	class Table {
+    	String name;
+    	Rows rows;
+	}
+	
+	
+	class Rows {
+		Set<String> columns;
+		List<Integer> data;
+	}
 }
