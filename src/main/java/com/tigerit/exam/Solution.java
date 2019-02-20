417a3d5f655f10cb2388d11a71d28448ad5f7537
Shahed Bin Satter <shahed.satter@datasoft-bd.com>
2019-02-20 12:20:34 +0600

    Take input and print tables

--- src/main/java/com/tigerit/exam/Solution.java
+++ src/main/java/com/tigerit/exam/Solution.java
@@ -2,6 +2,7 @@ package com.tigerit.exam;
 
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static com.tigerit.exam.IO.*;
 
@@ -13,26 +14,18 @@ import static com.tigerit.exam.IO.*;
  */
 public class Solution implements Runnable {
 	private static final int QUERY_MAX_LINE_NUM = 4;
+	private static final HashMap<String, Table> tables = new HashMap<>();
+	// Maintain the list of queries
+	private static final List<String> queries = new ArrayList<>();
 	
 	@Override
     public void run() {
-        // your application entry point
-
-        // sample input process
-//        String string = readLine();
-//
-//        Integer integer = readLineAsInteger();
-
-        // sample output process
-//        printLine(string);
-//        printLine(integer);
-	   
+		// application entry point
 	    Integer numberOfTestCases = readLineAsInteger();
-	    
-	    testCase(numberOfTestCases);
-	   
-	   
-	   
+	    // iterate over all the test cases
+		for (int caseNumber = 1; caseNumber <= numberOfTestCases; caseNumber++) {
+			testCase(caseNumber);
+		}
     }
 	
 	private void testCase(Integer caseNumber) {
@@ -41,13 +34,16 @@ public class Solution implements Runnable {
 		for (int tableNumber = 0; tableNumber < numberOfTables; tableNumber++) {
 			initiateTable(numberOfTables);
 		}
+		printLine("Done table initialization");
 		
 		Integer numberOfQueries = readLineAsInteger();
-		List<String> queries = new ArrayList<>();
+
 		for (int queryNumber = 0; queryNumber < numberOfQueries; queryNumber++) {
 			String query = collectQuery();
 			queries.add(query);
 		}
+		
+		printLine(queries);
 	}
 	
 	private String collectQuery() {
@@ -60,28 +56,95 @@ public class Solution implements Runnable {
 	}
 	
 	private void initiateTable(Integer numberOfTables) {
-		String tableName = readLine();
 		// dimensions -> row, cols
 		int[] dimensions = Arrays.stream(readLine().split("\\s")).mapToInt(Integer::parseInt).toArray();
+		Table table = new Table(readLine(), dimensions[0]);
+		
 		printLine(String.format("TEST: Dimensions: %d, %d", dimensions[0], dimensions[1]));
-		printLine(String.format("TEST: Table name: %s", tableName));
+		printLine(String.format("TEST: Table name: %s", table.getName()));
+		
 		String [] columnNames = readLine().split("\\s");
+		// todo check for optimization
+		List<String> columns = Arrays.stream(columnNames).collect(Collectors.toList());
+		table.setColumns(columns);
+		
+		
 		printLine("TEST: Collected column names");
 		for (int row = 0; row < dimensions[0]; row++) {
-			int[] rowData = Arrays.stream(readLine().split("\\s")).mapToInt(Integer::parseInt).toArray();
+			// todo check for optimization
+			String [] values = readLine().split("\\s");
+			Integer [] rowData = new Integer[values.length];
+			for (int idx = 0; idx < rowData.length; idx++) {
+				rowData[idx] = Integer.parseInt(values[idx]);
+			}
+			table.addRow(row, rowData);
 		}
-		
+		printLine("Table :" + table);
 		
 	}
 	
 	class Table {
     	String name;
-    	Rows rows;
+    	List<String> columns;
+    	Row [] rows;
+		
+		public Table(String name, Integer totalRows) {
+			this.name = name;
+			this.rows = new Row [totalRows];
+		}
+		
+		public List<String> getColumns() {
+			return columns;
+		}
+		
+		public void setColumns(List<String> columns) {
+			this.columns = columns;
+		}
+	    
+	    String getName() {
+    		return this.name;
+	    }
+	    
+	    void setName(String name) {
+    		this.name = name;
+	    }
+	    
+	    void addRow(Integer index, Integer [] data) {
+    		rows[index] = new Row(data);
+	    }
+		
+		@Override
+		public String toString() {
+			StringBuilder builder = new StringBuilder();
+			columns.forEach(col -> builder.append(col).append("\t"));
+			builder.append("\n");
+			Arrays.asList(rows).forEach(row -> builder.append(row).append("\n"));
+			return String.format("Table [%s]\n%s", this.name, builder.toString());
+		}
 	}
 	
 	
-	class Rows {
-		Set<String> columns;
+	class Row {
 		List<Integer> data;
+		
+		public Row() {}
+		public Row(Integer [] data) {
+			this.data = Arrays.asList(data);
+		}
+		
+		void setData(List<Integer> data) {
+			this.data = data;
+		}
+		
+		public List<Integer> getData() {
+			return data;
+		}
+		
+		@Override
+		public String toString() {
+			StringBuilder builder = new StringBuilder();
+			data.forEach(integer -> builder.append(integer).append("\t"));
+			return builder.toString();
+		}
 	}
 }
